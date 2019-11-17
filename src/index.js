4c898a6fff0bb120339d7975e44a3beb7395de80
slutske22 <seth.lutske@gmail.com>
2019-11-17 08:07:14 -0800

    modules not working really

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('the thing');
+import L from '../node_modules/leaflet'
+
+var mapOptions = {
+  renderer: L.canvas(),
+  center: [32.96176, -117.03529],
+  zoom: 16,
+  zoomDelta: 0.5,
+  zoomSnap: 0
+}
+
+const map = L.map('mapID', mapOptions)
+
+var mbStandardOutdoorLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox.outdoors',
+      accessToken: 'pk.eyJ1Ijoic2x1dHNrZTIyIiwiYSI6ImNqeGw1Y3BibDAybG4zeHFyaXl3OXVwZXUifQ.fZ_5Raq5z-DUpo2AK-bQHA'
+   }).addTo(map)
