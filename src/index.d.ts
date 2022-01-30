d586939671c83a6c95e5387f797178ac8350eae2
Seth Lutske <seth.lutske@gmail.com>
2022-01-30 07:31:07 -0800

    Fixing .d.ts file

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,4 @@
-import { PolylineOptions } from 'leaflet';
+import * as L from 'leaflet';
 
 interface SingleArrowheadOptions {
 	/**
@@ -11,11 +11,11 @@ interface SingleArrowheadOptions {
 	size?: `${number}px` | `${number}%` | `${number}m`;
 }
 
-interface ArrowheadOptions extends PolylineOptions, SingleArrowheadOptions {
+interface ArrowheadOptions extends L.PolylineOptions, SingleArrowheadOptions {
 	/**
 	 * The number and spacing of arrowheads to draw along the polyline
 	 */
-	frequency?: `${number}px` | `${number}m` | 'allvertices' | 'endonly';
+	frequency?: number | `${number}px` | `${number}m` | 'allvertices' | 'endonly';
 	/**
 	 * If the size of the arrowheads, when given in percent, should be a percentage proportional
 	 * to the total length of the polyline, rather than the average of all the segments
@@ -31,16 +31,18 @@ interface ArrowheadOptions extends PolylineOptions, SingleArrowheadOptions {
 	/**
 	 * Callback function to customize arrowheads on a 1-by-1 basis
 	 */
-	perArrowheadOptions?: (i: number) => PolylineOptions & SingleArrowheadOptions;
+	perArrowheadOptions?: (
+		i: number
+	) => L.PolylineOptions & SingleArrowheadOptions;
 }
 
-declare module L {
+declare module 'leaflet' {
 	export interface Polyline {
 		/**
 		 * Adds arrowheads to an L.polyline
 		 * @param {object} options The options for the arrowhead.  See documentation for details
 		 * @returns The L.polyline instance that they arrowheads are attached to
 		 */
-		arrowheads: (ArrowheadOptions) => Polyline;
+		arrowheads: (options: ArrowheadOptions) => L.Polyline;
 	}
 }
